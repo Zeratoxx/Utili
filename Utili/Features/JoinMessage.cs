859f69562b0dc42befe268a4491c69589c826320
Daniel Baynton <Daniel.Baynton@hotmail.com>
2020-05-03 12:24:00 +0100

    Ready for test

--- Utili/Features/JoinMessage.cs
+++ Utili/Features/JoinMessage.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Net;
-using System.Threading;
-using System.Text;
-
-using Discord;
+﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-
+using System.Linq;
+using System.Threading.Tasks;
 using static Utili.Data;
 using static Utili.Logic;
 using static Utili.SendMessage;
-using static Utili.Program;
-using static Utili.Json;
 
 namespace Utili
 {
@@ -24,12 +13,12 @@ namespace Utili
     {
         public async Task JoinMessage_UserJoined(SocketGuildUser User)
         {
-            if(GetData(User.Guild.Id.ToString(), "JoinMessage-Enabled", "true").Count > 0)
+            if (GetData(User.Guild.Id.ToString(), "JoinMessage-Enabled", "true").Count > 0)
             {
                 var JoinMessage = await GetJoinMessageAsync(User.Guild, User);
 
                 string ChannelData = GetData(User.Guild.Id.ToString(), "JoinMessage-Channel").FirstOrDefault().Value;
-                if(ulong.TryParse(ChannelData, out ulong ChannelID))
+                if (ulong.TryParse(ChannelData, out ulong ChannelID))
                 {
                     ITextChannel Channel = User.Guild.GetTextChannel(ChannelID);
                     await Channel.SendMessageAsync(JoinMessage.Item1, embed: JoinMessage.Item2);
@@ -42,7 +31,7 @@ namespace Utili
             }
         }
 
-        public async Task<(string, Embed)> GetJoinMessageAsync(SocketGuild Guild, SocketUser User)
+        public static async Task<(string, Embed)> GetJoinMessageAsync(SocketGuild Guild, SocketUser User)
         {
             string Title;
             try { Title = GetData($"{Guild.Id}", $"JoinMessage-Title").First().Value; }
@@ -98,7 +87,7 @@ namespace Utili
 
             EmbedBuilder Embed = new EmbedBuilder();
             if (Title != "") Embed.WithAuthor(new EmbedAuthorBuilder().WithName(Title));
-            if(Title == "" && ImageURL != "") Embed.WithAuthor(new EmbedAuthorBuilder().WithName("Title required for image!"));
+            if (Title == "" && ImageURL != "") Embed.WithAuthor(new EmbedAuthorBuilder().WithName("Title required for image!"));
             if (ImageURL != "") Embed.WithAuthor(Embed.Author.WithIconUrl(ImageURL));
 
             if (Footer != "") Embed.WithFooter(new EmbedFooterBuilder().WithText(Footer));
@@ -131,9 +120,10 @@ namespace Utili
                 "thumbnail [channel] [url | user | none] - Set the thumbnail URL of the join message\n" +
                 "footerImage [channel] [url | user | none] - Set the footer URL of the join message\n" +
                 "colour [channel] [R] [G] [B] - Set the colour of the join message\n\n" +
+                "channel [channel | dm] - Set the channel of the join messages\n" +
                 "preview - Send a preview the join message\n" +
-                "on [channel] - Enable the join message in a channel\n" +
-                "off [channel] - Disable the join message in a channel";
+                "on - Enable the join message\n" +
+                "off - Disable the join message";
 
         [Command("Help")]
         public async Task Help()
@@ -158,212 +148,221 @@ namespace Utili
         }
 
         [Command("SetTitle"), Alias("Title")]
-        public async Task SetTitle(ITextChannel Channel, [Remainder] string Content)
+        public async Task SetTitle([Remainder] string Content)
         {
-            if(Permission(Context.User, Context.Channel))
+            if (Permission(Context.User, Context.Channel))
             {
-                if(Content.ToLower() == "none")
+                if (Content.ToLower() == "none")
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Title-{Channel.Id}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Title");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Title removed"));
                 }
-                else if(Content.Length <= 500)
+                else if (Content.Length <= 500)
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Title-{Channel.Id}");
-                    SaveData($"{Context.Guild.Id}", $"JoinMessage-Title-{Channel.Id}", $"{Base64Encode(Content)}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Title");
+                    SaveData($"{Context.Guild.Id}", $"JoinMessage-Title", $"{Base64Encode(Content)}");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Title set"));
                 }
                 else { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Title can not exceed 500 characters")); }
 
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
+
+
             }
         }
 
         [Command("SetContent"), Alias("Content")]
-        public async Task SetContent(ITextChannel Channel, [Remainder] string Content)
+        public async Task SetContent([Remainder] string Content)
         {
             if (Permission(Context.User, Context.Channel))
             {
                 if (Content.ToLower() == "none")
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Content-{Channel.Id}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Content");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Content removed"));
                 }
                 else if (Content.Length <= 1500)
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Content-{Channel.Id}");
-                    SaveData($"{Context.Guild.Id}", $"JoinMessage-Content-{Channel.Id}", $"{Base64Encode(Content)}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Content");
+                    SaveData($"{Context.Guild.Id}", $"JoinMessage-Content", $"{Base64Encode(Content)}");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Content set"));
                 }
                 else { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Content can not exceed 1500 characters")); }
-
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
             }
         }
 
         [Command("SetFooter"), Alias("Footer")]
-        public async Task SetFooter(ITextChannel Channel, [Remainder] string Content)
+        public async Task SetFooter([Remainder] string Content)
         {
             if (Permission(Context.User, Context.Channel))
             {
                 if (Content.ToLower() == "none")
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Footer-{Channel.Id}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Footer");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Footer removed"));
                 }
                 else if (Content.Length <= 1500)
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Footer-{Channel.Id}");
-                    SaveData($"{Context.Guild.Id}", $"JoinMessage-Footer-{Channel.Id}", $"{Base64Encode(Content)}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-Footer");
+                    SaveData($"{Context.Guild.Id}", $"JoinMessage-Footer", $"{Base64Encode(Content)}");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Footer set"));
                 }
                 else { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Footer can not exceed 1500 characters")); }
-
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
             }
         }
 
         [Command("SetColour"), Alias("SetColor", "Colour", "Color")]
-        public async Task SetColour(ITextChannel Channel, int R, int G, int B)
+        public async Task SetColour(int R, int G, int B)
         {
             if (Permission(Context.User, Context.Channel))
             {
                 Color Colour;
                 try { Colour = new Color(R, G, B); }
                 catch { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Invalid colour", "[Pick colour](https://www.rapidtables.com/web/color/RGB_Color.html)\n[Support Discord](https://discord.gg/WsxqABZ)")); return; }
-                    
-                DeleteData($"{Context.Guild.Id}", $"JoinMessage-Colour-{Channel.Id}");
-                SaveData($"{Context.Guild.Id}", $"JoinMessage-Colour-{Channel.Id}", $"{R} {G} {B}");
-                await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Colour set", "Preview this colour in this embed.").ToEmbedBuilder().WithColor(Colour).Build());
 
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
+                DeleteData($"{Context.Guild.Id}", $"JoinMessage-Colour");
+                SaveData($"{Context.Guild.Id}", $"JoinMessage-Colour", $"{R} {G} {B}");
+                await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Colour set", "Preview this colour in this embed.").ToEmbedBuilder().WithColor(Colour).Build());
             }
         }
 
         [Command("SetIcon"), Alias("Icon", "SetAvatar", "Avatar")]
-        public async Task SetIcon(ITextChannel Channel, string ImageURL)
+        public async Task SetIcon(string ImageURL)
         {
             if (Permission(Context.User, Context.Channel))
             {
                 if (ImageURL.ToLower() == "none")
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-ImageURL-{Channel.Id}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-ImageURL");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Icon removed"));
                 }
                 else if (ImageURL.Length <= 1000)
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-ImageURL-{Channel.Id}");
-                    SaveData($"{Context.Guild.Id}", $"JoinMessage-ImageURL-{Channel.Id}", $"{Base64Encode(ImageURL)}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-ImageURL");
+                    SaveData($"{Context.Guild.Id}", $"JoinMessage-ImageURL", $"{Base64Encode(ImageURL)}");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Icon URL set", "Note that an invalid icon URL may result in the message not being sent at all"));
                 }
                 else { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Icon URL can not exceed 1000 characters")); }
-
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
             }
         }
 
         [Command("SetImage"), Alias("Image")]
-        public async Task SetImage(ITextChannel Channel, string ImageURL)
+        public async Task SetImage(string ImageURL)
         {
             if (Permission(Context.User, Context.Channel))
             {
                 if (ImageURL.ToLower() == "none")
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-LargeImageURL-{Channel.Id}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-LargeImageURL");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Image removed"));
                 }
                 else if (ImageURL.Length <= 1000)
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-LargeImageURL-{Channel.Id}");
-                    SaveData($"{Context.Guild.Id}", $"JoinMessage-LargeImageURL-{Channel.Id}", $"{Base64Encode(ImageURL)}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-LargeImageURL");
+                    SaveData($"{Context.Guild.Id}", $"JoinMessage-LargeImageURL", $"{Base64Encode(ImageURL)}");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Image URL set", "Note that an invalid image URL may result in the message not being sent at all"));
                 }
                 else { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Image URL can not exceed 1000 characters")); }
-
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
             }
         }
 
         [Command("SetThumbnail"), Alias("Thumbnail")]
-        public async Task SetThumbnail(ITextChannel Channel, string ImageURL)
+        public async Task SetThumbnail(string ImageURL)
         {
             if (Permission(Context.User, Context.Channel))
             {
                 if (ImageURL.ToLower() == "none")
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-ThumbnailURL-{Channel.Id}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-ThumbnailURL");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Thumbnail removed"));
                 }
                 else if (ImageURL.Length <= 1000)
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-ThumbnailURL-{Channel.Id}");
-                    SaveData($"{Context.Guild.Id}", $"JoinMessage-ThumbnailURL-{Channel.Id}", $"{Base64Encode(ImageURL)}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-ThumbnailURL");
+                    SaveData($"{Context.Guild.Id}", $"JoinMessage-ThumbnailURL", $"{Base64Encode(ImageURL)}");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Thumbnail URL set", "Note that an invalid thumbnail URL may result in the message not being sent at all"));
                 }
                 else { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Thumbnail URL can not exceed 1000 characters")); }
-
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
             }
         }
 
         [Command("SetFooterImage"), Alias("FooterImage")]
-        public async Task SetFooterURL(ITextChannel Channel, [Remainder] string ImageURL)
+        public async Task SetFooterURL([Remainder] string ImageURL)
         {
             if (Permission(Context.User, Context.Channel))
             {
                 if (ImageURL.ToLower() == "none")
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-FooterImageURL-{Channel.Id}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-FooterImageURL");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Footer image removed"));
                 }
                 else if (ImageURL.Length <= 1000)
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-FooterImageURL-{Channel.Id}");
-                    SaveData($"{Context.Guild.Id}", $"JoinMessage-FooterImageURL-{Channel.Id}", $"{Base64Encode(ImageURL)}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-FooterImageURL");
+                    SaveData($"{Context.Guild.Id}", $"JoinMessage-FooterImageURL", $"{Base64Encode(ImageURL)}");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Footer image set"));
                 }
                 else { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Footer image URL can not exceed 1000 characters")); }
-
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
             }
         }
 
         [Command("SetNormalText"), Alias("NormalText")]
-        public async Task SetNormalText(ITextChannel Channel, [Remainder] string Content)
+        public async Task SetNormalText([Remainder] string Content)
         {
             if (Permission(Context.User, Context.Channel))
             {
                 if (Content.ToLower() == "none")
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-NormalText-{Channel.Id}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-NormalText");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Normal text removed"));
                 }
                 else if (Content.Length <= 1500)
                 {
-                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-NormalText-{Channel.Id}");
-                    SaveData($"{Context.Guild.Id}", $"JoinMessage-NormalText-{Channel.Id}", $"{Base64Encode(Content)}");
+                    DeleteData($"{Context.Guild.Id}", $"JoinMessage-NormalText");
+                    SaveData($"{Context.Guild.Id}", $"JoinMessage-NormalText", $"{Base64Encode(Content)}");
                     await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Normal text set"));
                 }
                 else { await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Normal text can not exceed 1500 characters")); }
+            }
+        }
 
-                NoticeMessage NoticeMessage = new NoticeMessage();
-                await NoticeMessage.Update(Channel);
+        [Command("Channel")]
+        public async Task Channel(ITextChannel Channel)
+        {
+            if (Permission(Context.User as SocketGuildUser, Context.Channel))
+            {
+                if (BotHasPermissions(Channel, new ChannelPermission[] { ChannelPermission.ViewChannel, ChannelPermission.SendMessages, ChannelPermission.EmbedLinks }, Context.Channel))
+                {
+                    DeleteData(Context.Guild.Id.ToString(), "JoinMessage-Channel");
+                    SaveData(Context.Guild.Id.ToString(), "JoinMessage-Channel", Channel.Id.ToString());
+                    await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Join message channel set", $"Join messages will now be sent in {Channel.Mention}"));
+                }
+            }
+        }
+
+        [Command("Channel")]
+        public async Task Channel(string DM)
+        {
+            if(DM.ToLower() == "dm")
+            {
+                if (Permission(Context.User as SocketGuildUser, Context.Channel))
+                {
+                    DeleteData(Context.Guild.Id.ToString(), "JoinMessage-Channel");
+                    SaveData(Context.Guild.Id.ToString(), "JoinMessage-Channel", "DM");
+                    await Context.Channel.SendMessageAsync(embed: GetEmbed("Yes", "Join message channel set", $"Join messages will now be sent to new users via DMs"));
+                }
+            }
+            else
+            {
+                string Prefix = ".";
+                try { Prefix = Data.GetData(Context.Guild.Id.ToString(), "Prefix").First().Value; } catch { }
+                await Context.Channel.SendMessageAsync(embed: GetEmbed("No", "Invalid command syntax", $"Try {Prefix}help\n[Support Discord](https://discord.gg/WsxqABZ)"));
             }
         }
 
         [Command("Preview")]
-        public async Task Send()
+        public async Task Preview()
         {
-            NoticeMessage NM = new NoticeMessage();
-            await NM.Update(Context.Channel as ITextChannel, false);
+            await JoinMessage.GetJoinMessageAsync(Context.Guild, Context.User);
         }
 
         [Command("On"), Alias("Enable")]
